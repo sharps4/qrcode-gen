8ad5462bff242bc71d4c8972994853dfada83868
Adrian Lecorf <adrian.lecorf@gmail.com>
2023-06-17 17:34:56 +0200

    1st commit

--- qrcode_gen.py
+++ qrcode_gen.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file, send_from_directory
 import qrcode
+import zipfile
+import os
+
 
 app = Flask(__name__)
 
@@ -13,29 +16,44 @@ def generate():
     series_names = [request.form.get(f'series_name_{i}', f"Série {i+1}") for i in range(nb_series)]
     qrcodes_counts = [int(request.form.get(f'qrcodes_count_{i}', 0)) for i in range(nb_series)]
 
-    for serie in range(nb_series):
-        series_name = series_names[serie]
-        qrcodes_count = qrcodes_counts[serie]
+    zip_filename = 'qrcodes.zip'  # Nom du fichier ZIP
 
-        for compteur in range(1, qrcodes_count + 1):
-            prefixe = series_name
-            data = f"{prefixe} numéro: {compteur}"
-            filename = f"{prefixe}_{compteur}.png"
+    with zipfile.ZipFile(zip_filename, 'w') as zip_file:
+        for serie in range(nb_series):
+            series_name = series_names[serie]
+            qrcodes_count = qrcodes_counts[serie]
 
-            qr = qrcode.QRCode(
-                version=1,
-                error_correction=qrcode.constants.ERROR_CORRECT_L,
-                box_size=10,
-                border=4,
-            )
-            qr.add_data(data)
-            qr.make(fit=True)
+            for compteur in range(1, qrcodes_count + 1):
+                prefixe = series_name
+                data = f"{prefixe} numéro: {compteur}"
+                filename = f"{prefixe}_{compteur}.png"
 
-            img = qr.make_image(fill_color="black", back_color="white")
-            img.save(filename)
-            print(f"QR Code {compteur} généré : {filename}")
+                qr = qrcode.QRCode(
+                    version=1,
+                    error_correction=qrcode.constants.ERROR_CORRECT_L,
+                    box_size=10,
+                    border=4,
+                )
+                qr.add_data(data)
+                qr.make(fit=True)
 
-    return 'Les codes QR ont été générés avec succès !'
+                img = qr.make_image(fill_color="black", back_color="white")
+                img.save(filename)
+                print(f"QR Code {compteur} généré : {filename}")
+
+                img.save(filename)
+
+                # Ajouter le fichier au fichier ZIP
+                zip_file.write(filename)
+
+        return f'/download/{zip_filename}'
+
+        # return 'Les codes QR ont été générés avec succès !'
+
+@app.route('/download/<path:filename>', methods=['GET'])
+def download(filename):
+    directory = os.path.dirname(__file__)
+    return send_from_directory(directory, filename, as_attachment=True)
 
 if __name__ == '__main__':
     app.run(debug=True)
